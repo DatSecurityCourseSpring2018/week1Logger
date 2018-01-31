452d1adab6e0747c89d8bf13872f7efb19c5c75c
Kasper Osterbye <kasper@cphbusiness.dk>
2018-01-31 09:42:24 +0100

    Done for demo

--- src/main/java/logger/LogFile.java
+++ src/main/java/logger/LogFile.java
@@ -15,14 +15,16 @@ import static java.nio.file.StandardOpenOption.*;
  @author kasper
  */
 public class LogFile {
-    private final static Charset ENCODING = StandardCharsets.UTF_8;
-    private final static String LOG_PATH= "LOG_FILE";
+    private final static Charset ENCODING = StandardCharsets.UTF_8;   
+    private final static String LOG_PATH= "LOG_PATH";
     private final Path filePath;
     
     public LogFile() {
         String fp = System.getenv( LOG_PATH );
-        if (fp == null ) // While debugging in Netbeans TODO
-            fp = "/Users/kasper/debuglog.txt";
+        if (fp == null ) {
+            System.err.println( "LOG_PATH not defined" );
+            System.exit( 1 );
+        }
         System.out.println( "Logging to file: " + fp );
         filePath = Paths.get( fp );
     }
